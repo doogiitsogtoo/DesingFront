263db8ec4c83a8280d66aa9f5efec6e20b1099ac
doogiitsogtoo <doogiitsogtoo08@gmail.com>
2021-05-14 19:04:51 +0800

    first

---
+++ home.js
@@ -0,0 +1,9 @@
+// $(document).on('ready', function() {
+//     $('.slider-img').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear'
+//     });
+// });
